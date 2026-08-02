374da37d1ff33409d895d3f4fc6a1200b24f25d7
Tobi <tleingartner@icloud.com>
2026-08-02 12:54:12 +0200

    Update 2d_engine.py

--- 2d_engine.py
+++ 2d_engine.py
@@ -7,46 +7,6 @@ from pathlib import Path
 import shutil
 import os
 import subprocess
-"""
-import time
-import mss
-import numpy as np
-
-with mss.mss() as sct:
-    monitor = sct.monitors[1]
-
-    last = time.time()
-    frames = 0
-
-    while True:
-        img = np.array(sct.grab(monitor))
-        frames += 1
-
-        if time.time() - last >= 1:
-            print(f"FPS: {frames}")
-            frames = 0
-            last = time.time()
-
-
-import time
-import mss
-import numpy as np
-
-with mss.mss() as sct:
-    monitor = sct.monitors[1]
-
-    last = time.time()
-    frames = 0
-
-    while True:
-        img = np.array(sct.grab(monitor))
-        frames += 1
-
-        if time.time() - last >= 1:
-            print(f"FPS: {frames}")
-            frames = 0
-            last = time.time()
-"""
 
 repo_url = "https://github.com/IchbinTobbss/Tobbss-Framework.git"
 current_dir = Path(__file__).parent.resolve()
